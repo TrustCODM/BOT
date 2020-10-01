3aac2c821cac1c53e6409e34eedaf1b7cb5ba7cc
TrustCODM <72182162+TrustCODM@users.noreply.github.com>
2020-10-01 10:01:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.FVJkb4Q0H34xU4rshgrUi3XkOVvgh94m);//BOT_TOKEN is the Client Secret
